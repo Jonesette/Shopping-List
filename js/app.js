39e606ec6f5e39830c5388a86b6e082d08b17dea
Jonesette <sjones@jjaviation.com>
2014-05-05 22:46:44 -0600

    Working List
    Updated variables so they update when entered in form.

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
 	// Set Initial Variable Values
-	var description = "Description";
-	var quantity = +"3";
-	var price = +"5";
+	var description = "Add a description.";
+	var quantity = "0";
+	var price = "0";
 
 	//Clear Description on click
 	$('.description').one('focus', function() {
@@ -11,27 +11,27 @@ $(document).ready(function() {
 
 	// Get Description
 	$('.item-details').on('focusout', '.description', function() {
-		var description = $(this).val();
-		alert(description);
+		description = $(this).val();
+		//alert(description);
 	});
 
 	// Get Quantity
 	$('.item-details').on('focusout', '.quantity', function() {
-		var quantity = +$(this).val();
-		alert(quantity);
+		quantity = +$(this).val();
+		//alert(quantity);
 	});
 
 	// Get Price
 	$('.item-details').on('focusout', '.price', function() {
-		var price = +$(this).val();
-		alert(price);
+		price = +$(this).val();
+		//alert(price);
 	});
 
 	// Set Total equal to Quantity * Price
 	$('#add-item').click(function(event) {
 		event.preventDefault();
 		var itemtotal = quantity * price;
-		alert(itemtotal);
+		//alert(itemtotal);
 		// Show Table
 		$('.table').show();
 		// Append to Table
